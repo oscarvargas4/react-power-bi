99ea863292d7b35826fd621afbd09fb201918e17
Oscar Vargas <oscar.andres.vargas1@gmail.com>
2025-02-16 11:32:06 +0100

    Adding Power BI component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { PowerBIEmbed } from "powerbi-client-react";
+import { models } from 'powerbi-client'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <PowerBIEmbed
+          embedConfig={{
+            type: "report", // Supported types: report, dashboard, tile, visual, qna, paginated report and create
+            id: process.env.REACT_APP_REPORT_ID,
+            embedUrl: process.env.REACT_APP_EMBED_URL,
+            accessToken: process.env.REACT_APP_BI_TOKEN,
+            tokenType: models.TokenType.Aad, // Use models.TokenType.Aad for SaaS embed
+            settings: {
+              panes: {
+                filters: {
+                  expanded: true,
+                  visible: true,
+                },
+              },
+              background: models.BackgroundType.Default,
+            },
+          }}
+          eventHandlers={
+            new Map([
+              [
+                "loaded",
+                function () {
+                  console.log("Report loaded");
+                },
+              ],
+              [
+                "rendered",
+                function () {
+                  console.log("Report rendered");
+                },
+              ],
+              [
+                "error",
+                function (event) {
+                  console.log(event.detail);
+                },
+              ],
+              ["visualClicked", () => console.log("visual clicked")],
+              ["pageChanged", (event) => console.log(event)],
+            ])
+          }
+          cssClassName={"Embed-container"}
+          getEmbeddedComponent={(embeddedReport) => {
+            window.report = embeddedReport;
+          }}
+        />
       </header>
     </div>
   );
